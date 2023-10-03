ed0ccae842be433373fc7a9d104964004cec2c6b
Olufemi <olufemifamiloyo@gmail.com>
2023-10-02 23:22:58 -0400

    deploying web app files

---
+++ Config.py
@@ -0,0 +1,6 @@
+customhost  = "employee.cbsb30lzcqnm.us-east-1.rds.amazonaws.com"
+customhuser = "admin"
+customhpass = "waterworld"
+customdb    = "employee"
+custombucket = "addemployee1000"
+customregion = "us-east-1"
